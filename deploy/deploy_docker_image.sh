c42c6c2d7c1a0c5354416e8a9c515050bbe1ea41
MaxSa <maximbt@gmail.com>
2022-01-06 11:28:39 +0200

    Docker container remove command first

--- deploy/deploy_docker_image.sh
+++ deploy/deploy_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo docker container rm "$(sudo docker ps -a -q)" -f
+sudo docker container rm $(sudo docker ps -a -q) -f
 sudo docker tag wog:build_"$1" xamsa/wog_build_"$1"
 sudo docker push xamsa/wog_build_"$1"
 sleep 2
