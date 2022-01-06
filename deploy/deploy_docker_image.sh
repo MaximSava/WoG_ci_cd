1be2ed264a5ff2a96bce6277b49222a079eefce7
MaxSa <maximbt@gmail.com>
2022-01-06 10:12:00 +0200

    New deploy to docker hub script

--- deploy/deploy_docker_image.sh
+++ deploy/deploy_docker_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker tag wog:build_"$BUILD_TAG" xamsa/wog_build_"$BUILD_TAG"
-sudo docker push xamsa/wog_build_"$BUILD_TAG"
+sudo docker tag wog:build_$BUILD_TAG xamsa/wog_build_$BUILD_TAG
+sudo docker push xamsa/wog_build_$BUILD_TAG
 sleep 2
 sudo docker container rm "$(sudo docker ps -a -q)" -f
